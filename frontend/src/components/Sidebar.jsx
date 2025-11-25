16fde81adf80bcea6e1535e23ed41f24b87638fb
prajyot bhopale <prajyot1818@gmail.com>
2025-11-25 13:19:52 +0530

    some changes

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -9,7 +9,13 @@ const Sidebar = ({ isMenuOpen, setIsMenuOpen }) => {
   const [search, setSearch] = useState("");
 
   return (
-    <div className={`flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#24221A]/30 to-[#0E0000]/30 border-r border-[#860699]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-[1] ${!isMenuOpen && 'max-md:-translate-x-full'}`}>
+    <div
+      className={`flex flex-col h-screen min-w-72 p-5 
+  dark:bg-linear-to-b dark:from-[#24221A] dark:to-[#0E0000] 
+  border-r border-[#860699]/30 backdrop-blur-3xl 
+  transition-all duration-500 max-md:absolute left-0 z-10 
+  ${!isMenuOpen && "max-md:-translate-x-full"}`}
+    >
       {/* LOGO */}
       <img
         src={theme === "dark" ? assets.logo_full : assets.logo_full_dark}
@@ -18,7 +24,7 @@ const Sidebar = ({ isMenuOpen, setIsMenuOpen }) => {
       />
 
       {/* NEW CHAT BUTTON */}
-      <button className="flex justify-center items-center w-full py-2 mt-10 text-white bg-gradient-to-r from-[#A45F67] to-[#3D81F6] text-sm rounded-md cursor-pointer">
+      <button className="flex justify-center items-center w-full py-2 mt-10 text-white bg-linear-to-r from-[#A45F67] to-[#3D81F6] text-sm rounded-md cursor-pointer">
         <span className="mr-2 text-xl">+</span>
         New Chat
       </button>
@@ -36,7 +42,9 @@ const Sidebar = ({ isMenuOpen, setIsMenuOpen }) => {
       </div>
 
       {/* RECENT CHATS */}
-      {chats.length > 0 && <p className="mt-4 text-sm dark:text-white">Recent Chats</p>}
+      {chats.length > 0 && (
+        <p className="mt-4 text-sm dark:text-white">Recent Chats</p>
+      )}
 
       <div className="flex-1 overflow-y-scroll mt-3 text-sm space-y-3">
         {chats
@@ -48,7 +56,12 @@ const Sidebar = ({ isMenuOpen, setIsMenuOpen }) => {
               : chat.name.toLowerCase().includes(search.toLowerCase())
           )
           .map((chat) => (
-            <div onClick={() =>{navigate('/'); setSelectedChat(chat); setIsMenuOpen(false)}}
+            <div
+              onClick={() => {
+                navigate("/");
+                setSelectedChat(chat);
+                setIsMenuOpen(false);
+              }}
               key={chat._id}
               className="p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300 dark:border-[#80609F]/15 rounded-md cursor-pointer flex justify-between group dark:text-white"
             >
@@ -75,7 +88,8 @@ const Sidebar = ({ isMenuOpen, setIsMenuOpen }) => {
       {/* community images */}
       <div
         onClick={() => {
-          navigate("/community");  setIsMenuOpen(false)
+          navigate("/community");
+          setIsMenuOpen(false);
         }}
         className="flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-105 transition-all"
       >
@@ -90,7 +104,8 @@ const Sidebar = ({ isMenuOpen, setIsMenuOpen }) => {
       {/* credits part */}
       <div
         onClick={() => {
-          navigate("/credits"); setIsMenuOpen(false)
+          navigate("/credits");
+          setIsMenuOpen(false);
         }}
         className="flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-105 transition-all"
       >
@@ -122,19 +137,25 @@ const Sidebar = ({ isMenuOpen, setIsMenuOpen }) => {
           <span className="absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition-transform peer-checked:translate-x-4"></span>
         </label>
       </div>
-          {/* user account */}
-      <div className="flex items-center gap-3 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer group"
-      >
-        <img
-          src={assets.user_icon}
-          className="w-7 rounded-full"
-          alt=""
-        />
-        <p className="flex-1 text-sm dark:text-primary truncate">{user ? user.name: 'login your account'} 
+      {/* user account */}
+      <div className="flex items-center gap-3 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer group">
+        <img src={assets.user_icon} className="w-7 rounded-full" alt="" />
+        <p className="flex-1 text-sm dark:text-primary truncate">
+          {user ? user.name : "login your account"}
         </p>
-        {user && <img src={assets.logout_icon} className="h-5 cursor-pointer hidden not-dark:invert group-hover:block"/>}
+        {user && (
+          <img
+            src={assets.logout_icon}
+            className="h-5 cursor-pointer hidden not-dark:invert group-hover:block"
+          />
+        )}
       </div>
-      <img onClick={()=> setIsMenuOpen(false) } src={assets.close_icon} className="absolute top-3 right-3 w-5 h-5 cursor-pointer md:hidden not-dark:invert" alt="" />
+      <img
+        onClick={() => setIsMenuOpen(false)}
+        src={assets.close_icon}
+        className="absolute top-3 right-3 w-5 h-5 cursor-pointer md:hidden not-dark:invert"
+        alt=""
+      />
     </div>
   );
 };
